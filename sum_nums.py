3d7dcb7795ee70b6117ea5e45c1e55a6ddaebf8f
Jordina Gavalda <jordinagavalda@gmail.com>
2020-05-07 17:36:49 +0200

    Now it works

--- sum_nums.py
+++ sum_nums.py
@@ -1,5 +1,15 @@
 data = [2, 4, 1]
-sum = 5
+data1 = [3,1,6]
+sum = 8
 
-def findsum(data):
-    pass
+def findsum(data, data1, sum):
+    for i in data:
+        for j in data1:
+            x = j+i
+            if x == sum:
+                print(f'{i} + {j} = {sum}')
+                quit()
+    else:
+        print('no such operation in this array')
+
+findsum(data, data1, sum)
