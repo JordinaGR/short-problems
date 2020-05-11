8164994b1de11b60b634ea0400a3a93875f468fd
Jordina Gavalda <jordinagavalda@gmail.com>
2020-05-11 13:57:40 +0200

    New files!

---
+++ printSquare.py
@@ -0,0 +1,9 @@
+w = 6
+n = 0
+
+while n < w:
+	print('\n')
+	for _ in range(w):
+		print('*', end='   ')
+
+	n += 1
--- repited_characters.py
+++ repited_characters.py
@@ -1,6 +1,33 @@
-data = "ABCB"
+data = "get_repited_characters"
 arr = []
 
-for i in data:
-    arr.append(i)
+for j in data:
+	arr.append(j)
 
+print('String to be analized: ')
+for i in arr:
+	print(i, end='')
+print('\n')
+
+arr.sort()
+
+print('Sorted string: ')
+for i in arr:
+	print(i, end=' ')
+print('\n')
+
+print('Repited characters: ')
+def main(arr):
+	if len(arr) <= 1 or len(arr) <= 0:
+		quit()
+	elif arr[0] != arr[1]:
+		del arr[0]
+		return main(arr)
+
+	elif arr[0] == arr[1]:
+		print(arr[0], end=', ')
+		del arr[0]
+		del arr[0]
+		return main(arr)
+
+main(arr)
