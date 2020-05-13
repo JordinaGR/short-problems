8004929e390d49733393b311278fa0092de5c69d
Jordina Gavalda <jordinagavalda@gmail.com>
2020-05-13 14:39:13 +0200

    Try except added

--- binarysearch.py
+++ binarysearch.py
@@ -1,10 +1,15 @@
 def binary_search(data, val):
     if len(data)==0 or (len(data)==1 and data[0]!=val):
-        print('false')
-    mid = data[len(data)//2]
-    if mid == val: print('true')
+        print('false, not found', val)
+    try:
+        mid = data[len(data)//2]
+    except:
+        print('Error, try again')
+        quit()
+
+    if mid == val: print('true', val)
     if mid>val: return binary_search(data[:len(data)//2], val)
     if mid<val: return binary_search(data[len(data)//2+1:], val)
 
 a = [1,2,4,5,6,7,8,9]
-binary_search(a,7)
+binary_search(a, 8)
