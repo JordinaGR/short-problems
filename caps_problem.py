cb7ff8bc03a1d5c288fe560cbdacfce0f1ecc14e
Jordina Gavalda <jordinagavalda@gmail.com>
2020-05-17 16:17:54 +0200

    explained the problem

--- caps_problem.py
+++ caps_problem.py
@@ -1,4 +1,7 @@
-data = ['F', 'B','B', 'F', 'B', 'F','B', 'F']
+# This is a solution for this problem: https://youtu.be/14UlXIZzwE4 Instructor: Srini Devadas
+# This is the solution going one time through the array.
+
+data = ['F', 'B', 'B', 'F', 'B', 'F', 'B', 'F']
 
 for i in range(len(data)-1):
     if data[i] != data[i+1]:
