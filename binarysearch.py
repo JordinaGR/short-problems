617290044eb4407f1b5ef5b0d5856a10bec5af83
Jordina Gavalda <jordinagavalda@gmail.com>
2020-06-25 21:13:52 +0200

    improved binary search

--- binarysearch.py
+++ binarysearch.py
@@ -2,16 +2,16 @@
 
 def binary_search(data, val):
     if len(data)==0 or (len(data)==1 and data[0]!=val): #When there are no values or one that that's not our value
-        print('false, not found', val)  #the value is not here and quit
-    try:
-        mid = data[len(data)//2]    #here i get the midpoin of the array
-    except:
-        print('Error, try again')
+        print("false")  #the value is not here and quit
         quit()
 
-    if mid == val: print('true', val)   #value found
-    if mid>val: return binary_search(data[:len(data)//2], val)  # It returns half of the array (where val is)
-    if mid<val: return binary_search(data[len(data)//2+1:], val)
+    mid = data[len(data)//2]    #here i get the midpoin of the array
+
+    if mid == val:
+        print("True")   #value found
+        quit()
+    if mid > val: return binary_search(data[:len(data)//2], val)  # It returns half of the array (where val is)
+    if mid < val: return binary_search(data[len(data)//2+1:], val)
 
 a = [1,2,4,5,6,7,8,9]   #Any sorted array
-binary_search(a, 8) #Call the function
+binary_search(a, 5) #Call the function
